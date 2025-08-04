cdfd98da0b883d572c130fc2824d48e0861012a9
songzhw <songzhw2012@gmail.com>
2025-08-03 23:32:58 -0400

    init proj

--- common/src/main/java/ca/six/router/demo/common/core/user/User.kt
+++
@@ -1,8 +0,0 @@
-package ca.six.router.demo.common.core.user
-
-// username could be 'vipay", "nopay", "normal", ...
-data class User(
-    var name: String,
-    var isVip: Boolean,
-    var payMethods: ArrayList<String>? = null
-)
--- library/src/main/java/ca/six/router/library/Precondition.kt
+++
@@ -1,7 +0,0 @@
-package ca.six.router.library
-
-// precondition满足就直接继续往下走, 所以没有successStation这一参数了
-class Precondition(
-    val precondition: () -> Boolean,
-    val destinationOnFail: String
-)
